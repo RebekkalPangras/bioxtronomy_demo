90961c4055564134ea48ced917571c1427b71c4a
Rebekkal <rebeccapancras@gmail.com>
2020-10-28 10:09:45 +0530

    updated changes for data loading

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -53,7 +53,7 @@ args = vars(ap.parse_args())
 # and batch size
 EPOCHS = 20
 INIT_LR = 1e-3
-BS = 32
+BS = 2
 
 # load the MNIST dataset
 print("[INFO] loading dataset...")
@@ -67,7 +67,7 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 from keras.preprocessing.image import ImageDataGenerator
 
-datagen = ImageDataGenerator()
+datagen = ImageDataGenerator(rescale=1./255)
 
 batch_size = 2
 img_height = 180
@@ -75,11 +75,11 @@ img_width = 180
 
 ...
 # load and iterate training dataset
-trainX = datagen.flow_from_directory('/content/bioxtronomy/data')
+trainX = datagen.flow_from_directory('/content/bioxtronomy/data', batch_size=3)
 # # load and iterate validation dataset
 # val_it = datagen.flow_from_directory('data/validation/', class_mode='binary', batch_size=64)
 # load and iterate test dataset
-testX = datagen.flow_from_directory('/content/bioxtronomy/data/astronomy')
+# testX = datagen.flow_from_directory('/content/bioxtronomy/data/astronomy')
 # data_dir = '/content/bioxtronomy/data'
 #
 # train_ds = tf.keras.preprocessing.image_dataset_from_directory(
@@ -104,9 +104,9 @@ testX = datagen.flow_from_directory('/content/bioxtronomy/data/astronomy')
 
 # add a channel dimension to every image in the dataset, then scale
 # the pixel intensities to the range [0, 1]
-trainX = np.expand_dims(trainX, axis=-1)
+# trainX = np.expand_dims(trainX, axis=-1)
 # testX = np.expand_dims(testX, axis=-1)
-trainX = trainX.astype("float32") / 255.0
+# trainX = trainX.astype("float32") / 255.0
 # testX = testX.astype("float32") / 255.0
 
 # construct our convolutional autoencoder
@@ -126,21 +126,21 @@ H = autoencoder.fit(
 # testing images, construct the visualization, and then save it
 # to disk
 print("[INFO] making predictions...")
-decoded = autoencoder.predict(testX)
-vis = visualize_predictions(decoded, testX)
-cv2.imwrite(args["vis"], vis)
+# decoded = autoencoder.predict(testX)
+# vis = visualize_predictions(decoded, testX)
+# cv2.imwrite(args["vis"], vis)
 
 # construct a plot that plots and saves the training history
-N = np.arange(0, EPOCHS)
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(N, H.history["loss"], label="train_loss")
-plt.plot(N, H.history["val_loss"], label="val_loss")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig(args["plot"])
+# N = np.arange(0, EPOCHS)
+# plt.style.use("ggplot")
+# plt.figure()
+# plt.plot(N, H.history["loss"], label="train_loss")
+# plt.plot(N, H.history["val_loss"], label="val_loss")
+# plt.title("Training Loss and Accuracy")
+# plt.xlabel("Epoch #")
+# plt.ylabel("Loss/Accuracy")
+# plt.legend(loc="lower left")
+# plt.savefig(args["plot"])
 
 # serialize the autoencoder model to disk
 print("[INFO] saving autoencoder...")
